d6900cd40307836dc60a324c64dedafac750a1fc
diepan-mindx <diepau1312@mindx.net.vn>
2025-02-20 20:18:14 +0700

    update

---
+++ js/ui.js
@@ -0,0 +1,11 @@
+const container = document.querySelecttor('.container');
+const registerBtn = document.querySelecttor('.register-btn');
+const loginloginBtn = document.querySelecttor('.login-btn');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+});
